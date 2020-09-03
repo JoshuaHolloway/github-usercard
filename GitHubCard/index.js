ecc8ccfb5aa6e2f596e597a3cba9228ffed77148
JoshuaHolloway <jhollow6@asu.edu>
2020-09-03 16:17:32 -0500

    constructing component with api data

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,17 +6,19 @@ const { default: Axios } = require("axios");
     https://api.github.com/users/<your name>
 */
 (function _axios_() {
-  Axios.get('https://api.github.com/users/JoshuaHolloway')
+  // Axios.get('https://api.github.com/users/a')
+  Axios.get('https://swapi.dev/api/people/1')
     .then(data => {
       console.log('axios data received: ', data.data);
     })
     .catch(err => {
       debugger;
     });
-})();
+});
 
 (function _fetch_() {
-  fetch('https://api.github.com/users/JoshuaHolloway')
+  fetch('https://api.github.com/users/a')
+  // fetch('https://swapi.dev/api/people/1')
     .then(partOfTheResponse => {
       // fetch is sooo eager to give sth quick,
       // it resolves some data before the body is in
@@ -24,6 +26,9 @@ const { default: Axios } = require("axios");
     })
     .then(jsonStuff => {
       console.log('fetch json-data received: ', jsonStuff);
+      compMaker(jsonStuff);
+
+
     })
     .catch(err => {
       debugger;
@@ -89,3 +94,106 @@ const followersArray = [];
     luishrd
     bigknell
 */
+function compMaker(obj) {
+  const ce = x => document.createElement(x);
+  const qs = x => document.querySelector(x);
+  const pr = x => console.log(x);
+  const ac = (x, _class_) => x.classList.add(_class_);
+
+    // <div class="card">
+    //   <img src={image url of user} />
+    //   <div class="card-info">
+    //     <h3 class="name">{users name}</h3>
+    //     <p class="username">{users user name}</p>
+    //     <p>Location: {users location}</p>
+    //     <p>Profile:
+    //       <a href={address to users github page}>{address to users github page}</a>
+    //     </p>
+    //     <p>Followers: {users followers count}</p>
+    //     <p>Following: {users following count}</p>
+    //     <p>Bio: {users bio}</p>
+    //   </div>
+    // </div>
+    const div_card = ce('div');
+    ac(div_card, 'card');
+    
+    const img = ce('img');
+    img.setAttribute('src', obj.avatar_url);
+    
+    //   <div class="card-info">
+    const div_card_info = ce('div');
+    ac(div_card_info, 'card-info');
+    
+    //     <h3 class="name">{users name}</h3>
+    const h3_name = ce('h3');
+    ac(h3_name, 'name');
+    h3_name.textContent = obj.name;   
+
+    //     <p class="username">{users user name}</p>
+    const p_username = ce('p');
+    ac(p_username, 'username');
+    p_username.textContent = obj.login;
+
+
+    //     <p>Location: {users location}</p>
+    const p_location = ce('p');
+    pr('debug:');
+    p_location.textContent = `Location: ${obj.location}`;
+    pr(p_location);
+
+    //     <p>Profile:
+    const p_profile = ce('p');
+    p_profile.textContent = 'Profile: ';
+
+    //       <a href={address to users github page}>{address to users github page}</a>
+    const a_tag = ce('a');
+    a_tag.setAttribute('href', obj.url);
+    a_tag.textContent = obj.url;
+    //     </p>
+
+    //     <p>Followers: {users followers count}</p>
+    const p_followers = ce('p');
+    p_followers.textContent = `Followers: ${obj.followers}`;
+
+      //     <p>Following: {users following count}</p>
+    const p_following = ce('p');
+    p_following.textContent = `Following: ${obj.following}`;
+
+    //     <p>Bio: {users bio}</p>
+    const p_bio = ce('p');
+    p_bio.textContent = `Bio: ${obj.bio}`;
+
+    // <div class="card">
+    //   <img src={image url of user} />
+    //   <div class="card-info">
+    //     <h3 class="name">{users name}</h3>
+    //     <p class="username">{users user name}</p>
+    //     <p>Location: {users location}</p>
+    //     <p>Profile:
+    //       <a href={address to users github page}>{address to users github page}</a>
+    //     </p>
+    //     <p>Followers: {users followers count}</p>
+    //     <p>Following: {users following count}</p>
+    //     <p>Bio: {users bio}</p>
+    //   </div>
+    // </div>  
+    
+
+
+    div_card_info.appendChild(h3_name);
+    div_card_info.appendChild(p_username);
+    div_card_info.appendChild(p_location);
+    p_profile.    appendChild(a_tag);
+    div_card_info.appendChild(p_profile);
+    div_card_info.appendChild(p_followers);
+    div_card_info.appendChild(p_following);
+    div_card_info.appendChild(p_bio);   
+    div_card.     appendChild(img);
+    div_card.     appendChild(div_card_info);
+    
+    pr('DIV_CARD:');
+    pr('DIV_CARD:');
+    pr('DIV_CARD:');
+    pr('DIV_CARD:');
+    pr(div_card);
+};
